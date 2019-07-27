fd8078a8500c1812624d71f94d2812400531af2a
Bryan Hayek <bryan.hayek@gmail.com>
2019-07-27 14:45:35 -0500

    respective sound is playing when a color is clicked

--- s.js
+++ s.js
@@ -1,1 +1,20 @@
-console.log("hello world")
+const playSound = (e) => {
+    console.log(e)
+    console.log(button.getAttribute('data-sound'))
+    //const audio = document.querySelector(`audio[id=${this.data-sound}]`)
+} 
+
+buttons = Array.from(document.querySelectorAll('.innerButton'))
+
+buttons.forEach(button => {
+    button.addEventListener('click', function(e){
+        soundId = button.getAttribute('data-sound');
+        const audio = document.querySelector(`audio[id=${soundId}]`)
+        audio.currentTime = 0;
+        audio.play()
+    })   
+})
+
+
+
+
